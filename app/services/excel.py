ea5d7691cc9f41886c959d58bb5d3fd3d3d01abf
FreeGMan <freegman74@gmail.com>
2026-02-24 09:07:55 +0100

    В модуль работы с Excel добавлено присвоение вормата для ячеек - Для ячеек с лидирующими нолями формат ячейки меняется на текстовый - Для ячеек с датой в формате ISO убирается тайм-зона и формат приводится просто к дате - Релазиция определения через регулярки

--- app/services/excel.py
+++ app/services/excel.py
@@ -1,5 +1,7 @@
 import os
+import re
 from openpyxl import Workbook
+from datetime import datetime
 from app.config import settings
 from app.logger import get_logger
 
@@ -13,6 +15,11 @@ def create_deal_report(smart_process_id: int, deals_data: list) -> str:
     Возвращает имя созданного файла.
     """
     try:
+        # Выражение для поиска строк с лидирующими нолями
+        re_leading_zero = re.compile(r'^0\d+$')
+        # Выражение для определение даты по ISO
+        re_iso_date = re.compile(r'^\d{4}-\d{2}-\d{2}T')
+
         filename = f"ticket_register_{smart_process_id}.xlsx"
         filepath = os.path.join(settings.files_dir, filename)
 
@@ -22,6 +29,25 @@ def create_deal_report(smart_process_id: int, deals_data: list) -> str:
 
         for deal_data in deals_data:
             ws.append(deal_data)
+           
+            # Переопределяем форматы в ячейках
+            for cell in ws[ws.max_row]:
+                val = str(cell.value) if cell.value else ""
+
+                # Число с лидирующими нолями
+                if re_leading_zero.match(val):
+                    cell.number_format = '@'  # Формат Текст
+                # Дата в формате ISO
+                elif re_iso_date.match(val):
+                    try:
+                        # Убираем таймзону для Excel и проставляем формат без точного времени
+                        cell.value = datetime.fromisoformat(val).replace(tzinfo=None)
+                        cell.number_format = 'DD.MM.YYYY' 
+                    except ValueError:
+                        # Если формат похож, но дата некорректная
+                        logger.warning(f"Unable to convert date from ISO string for value {val}")
+                        pass
+
 
         wb.save(filepath)
         logger.info(f"Excel report created: {filepath}")
